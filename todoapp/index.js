b209caa0723e80d58b0aeca056cf006c00389b22
Nguyễn Trường An <annobita1601@gmail.com>
2021-06-30 20:51:24 +0700

    add

---
+++ todoapp/index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+var server = require('http').Server(app);
+var mysql = require('mysql');
+var port = (process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 6969);
+server.listen(port, () => console.log('Server running in port ' + port));
+
+app.get('/', (req, res) => {
+    res.send("Home page. Server running okay.");
+    console.log("connect");
+})
